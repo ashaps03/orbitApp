bb0e47aae1a4aea17a6bc449a3464c9dde235f19
Rumman Shahzad <rumman@devasp.com>
2025-12-23 13:03:00 -0500

    Ticket#4: Fix typo in Firebase env var name

--- app/services/firebase.ts
+++ app/services/firebase.ts
@@ -9,7 +9,7 @@ const firebaseConfig = {
     projectId: process.env.EXPO_PUBLIC_FIREBASE_PROJECT_ID,
     storageBucket: process.env.EXPO_PUBLIC_FIREBASE_STORAGE_BUCKET,
     messagingSenderId: process.env.EXPO_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-    appId: process.env.EXPO_PUBLIC_FIREABSE_APP_ID,
+    appId: process.env.EXPO_PUBLIC_FIREBASE_APP_ID,
 };
 
 const app = getApps().length ? getApp() : initializeApp(firebaseConfig);
